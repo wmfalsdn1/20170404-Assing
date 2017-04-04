d76a0100b67f936ef1f13502a530f53925081e89
wmfalsdn1 <wmfalsdn124@naver.com>
2017-04-04 13:23:48 +0900

    두파일 추가
    두파일 추가

---
+++ javaOO/hyejichoi.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+package hyejichoi;
+
+public class hyejichoi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random r = new Random();
+		int number = r.nextInt(100);
+		System.out.println(number);
+		
+	}
+
+	
+}
